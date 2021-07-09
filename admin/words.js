1dc0cac502d8be632c95a7ee7ddf551301ce8269
ioBroker Adapter Creator <noreply@iobroker.net>
2021-07-09 13:25:05 +0200

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,77 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'apsystems adapter settings': {
+        'en': 'Adapter settings for apsystems',
+        'de': 'Adaptereinstellungen für apsystems',
+        'ru': 'Настройки адаптера для apsystems',
+        'pt': 'Configurações do adaptador para apsystems',
+        'nl': 'Adapterinstellingen voor apsystems',
+        'fr': "Paramètres d'adaptateur pour apsystems",
+        'it': "Impostazioni dell'adattatore per apsystems",
+        'es': 'Ajustes del adaptador para apsystems',
+        'pl': 'Ustawienia adaptera dla apsystems',
+        'zh-cn': 'apsystems的适配器设置'
+    },
+    'emaCloudUsername': {
+        'en': 'EMA-Cloud Username',
+        'de': 'EMA-Cloud-Benutzername',
+        'ru': 'Имя пользователя EMA-Cloud',
+        'pt': 'EMA-Cloud Nome de usuário',
+        'nl': 'EMA-Cloud gebruikersnaam',
+        'fr': "Nom d'utilisateur EMA-Cloud",
+        'it': 'Nome utente EMA-Cloud',
+        'es': 'Nombre de usuario de EMA-Cloud',
+        'pl': 'Nazwa użytkownika EMA-Cloud',
+        'zh-cn': 'EMA-Cloud 用户名'
+    },
+    'emaCloudPassword': {
+        'en': 'EMA-Cloud Password',
+        'de': 'EMA-Cloud-Passwort',
+        'ru': 'Пароль EMA-Cloud',
+        'pt': 'EMA-Cloud Password',
+        'nl': 'EMA-Cloud-wachtwoord',
+        'fr': 'Mot de passe EMA-Cloud',
+        'it': 'Password cloud EMACloud',
+        'es': 'Contraseña de EMA-Cloud',
+        'pl': 'Hasło do chmury EMA',
+        'zh-cn': 'EMA-Cloud 密码'
+    },
+    'dataSource': {
+        'en': 'Data source',
+        'de': 'Datenquelle',
+        'ru': 'Источник данных',
+        'pt': 'Fonte de dados',
+        'nl': 'Databron',
+        'fr': 'La source de données',
+        'it': 'Fonte di dati',
+        'es': 'Fuente de datos',
+        'pl': 'Źródło danych',
+        'zh-cn': '数据源'
+    },
+    'ecuId': {
+        'en': 'ECU ID',
+        'de': 'ECU-ID',
+        'ru': 'Идентификатор ЭБУ',
+        'pt': 'ECU ID',
+        'nl': 'ECU-ID',
+        'fr': "Identifiant de l'ECU",
+        'it': 'ID ECU',
+        'es': 'ID de la ECU',
+        'pl': 'Identyfikator ECU',
+        'zh-cn': 'ECU ID'
+    },
+    'ecuIP': {
+        'en': 'ECU WLAN IP',
+        'de': 'Steuergerät WLAN-IP',
+        'ru': 'ЭБУ WLAN IP',
+        'pt': 'IP WLAN ECU',
+        'nl': 'ECU WLAN IP',
+        'fr': "IP WLAN de l'ECU",
+        'it': 'IP WLAN ECU',
+        'es': 'ECU WLAN IP',
+        'pl': 'ECU WLAN IP',
+        'zh-cn': 'ECU WLAN IP'
+    }
+};
